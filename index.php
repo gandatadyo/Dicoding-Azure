96f06ff20932f6c44352b6c1017ae82c04fff18f
gandatadyo <gandatadyosurya@yahoo.com>
2019-03-22 06:29:38 +0700

    Update 22 Maret 2019

--- index.php
+++ index.php
@@ -1,33 +1,84 @@
-<?php
-    // echo "Data";
-?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Dicoding</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
-    <h1>Hello World</h1>
-    <button onclick="sayhi()">Hi</button>
-    <br>
-    <form action="uploadsystem.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
+    <h1>Final Project Class Azure</h1>
+    <form action="uploadsystem.php?" method="post" enctype="multipart/form-data">
+       <h5>Select file and upload to Blob Storage</h5>
         <input type="file" name="fileToUpload" id="fileToUpload">
         <input type="submit" value="Upload Image" name="submit">
     </form>
+    <hr>
+    <h5>Enter the URL to an image, then click the button. </h5>
+    Image to analyze:
+    <input type="text" name="inputImage" id="inputImage"
+        value="http://upload.wikimedia.org/wikipedia/commons/3/3c/Shaki_waterfall.jpg" />
+    <button onclick="processImage()">Analyze image</button>
     <br>
-    <form action="upload_tmp.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
+    Name image from response vision : <input type="text" id="idresponse" disabled >
+    <br>
+    <div id="wrapper" style="width:1020px; display:table;">
+        <div id="imageDiv" style="width:420px; display:table-cell;">
+            Source image:
+            <br><br>
+            <img id="sourceImage" width="400" height="250px" src="files/background.png"/>
+        </div>
+        <div id="jsonOutput" style="width:600px; display:table-cell;">
+            Response:
+            <br><br>
+            <textarea id="responseTextArea" class="UIInput"
+                    style="width:420px; height:250px;"></textarea>
+        </div>
+       
+    </div>
 
-    <script>
-        function sayhi() {  
-            alert('Hi !');
+    <script type="text/javascript">
+        function processImage() {
+            document.getElementById('idresponse').value = "";
+            var subscriptionKey = "db429da0b80a4760959c428f7af9fb96";
+            var uriBase ="https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+            var params = {
+                "visualFeatures": "Categories,Description,Color",
+                "details": "",
+                "language": "en",
+            };
+    
+            // Display the image.
+            var sourceImageUrl = document.getElementById("inputImage").value;
+            document.querySelector("#sourceImage").src = sourceImageUrl;
+    
+            // Make the REST API call.
+            $.ajax({
+                url: uriBase + "?" + $.param(params),
+                // Request headers.
+                beforeSend: function(xhrObj){
+                    xhrObj.setRequestHeader("Content-Type","application/json");
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+                },
+                type: "POST",
+                data: '{"url": ' + '"' + sourceImageUrl + '"}',
+            }).done(function(data) {
+                // Show formatted JSON on webpage.
+                $("#responseTextArea").val(JSON.stringify(data.categories[0], null, 2));
+                getNameImageVision(data);
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                // Display error message.
+                var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+                errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+                alert(errorString);
+            });
+        };
+
+        function getNameImageVision(obj) {  
+            document.getElementById('idresponse').value = obj.categories[0].name;
         }
     </script>
 </body>
--- uploadsystem.php
+++ uploadsystem.php
@@ -1,67 +1,136 @@
 <?php
-	require_once 'vendor/autoload.php';
-	require_once "./random_string.php";
+require_once 'vendor/autoload.php';
+require_once "./random_string.php";
 
-	use MicrosoftAzure\Storage\Blob\BlobRestProxy;
-	use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
-	use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
-	use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
-	use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-	$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
-	$blobClient = BlobRestProxy::createBlobService($connectionString);
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
+$blobClient = BlobRestProxy::createBlobService($connectionString);
 
-	$fileToUpload = "HelloWorld.txt";
-	// $fileToUpload =  basename($_FILES["fileToUpload"]["name"]);
 
-	$createContainerOptions = new CreateContainerOptions();
-	$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+function removeExt($path)
+{
+    $basename = basename($path);
+    return strpos($basename, '.') === false ? $path : substr($path, 0, - strlen($basename) + strlen(explode('.', $basename)[0]));
+}
 
-	// Set container metadata.
-	$createContainerOptions->addMetaData("key1", "value1");
-	$createContainerOptions->addMetaData("key2", "value2");
-	$containerName = "blockblobs".generateRandomString();
+if (!isset($_GET["Cleanup"])) {
+	// $fileToUpload = "donuts.png";
+	$fileToUpload = basename($_FILES["fileToUpload"]["name"]) ;
+	$fileToUplloadWithoutExtension = removeExt($_FILES["fileToUpload"]["name"]);
+	// echo "Name File : ".$fileToUplloadWithoutExtension."<br/>";
+	$uploadOk = 1;
+	$target_dir = "files/";
+	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+	$urlfile = "files/".basename( $_FILES["fileToUpload"]["name"]);
 
-	try {
-	    $blobClient->createContainer($containerName, $createContainerOptions);
-	    $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-	    fclose($myfile);
-	    
-	    echo "Uploading BlockBlob: ".PHP_EOL;
-	    echo $fileToUpload;
-	    echo "<br />";
-	    
-	    $content = fopen($fileToUpload, "r");
-	    $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-	    $listBlobsOptions = new ListBlobsOptions();
-	    $listBlobsOptions->setPrefix("HelloWorld");
-
-	    echo "These are the blobs present in the container: ";
-
-	    do{
-	        $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-	        foreach ($result->getBlobs() as $blob)
-	        {
-	            echo $blob->getName().": ".$blob->getUrl()."<br />";
-	        }
-	        $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-	    } while($result->getContinuationToken());
-	    echo "<br />";
-
-	    echo "This is the content of the blob uploaded: ";
-	    $blob = $blobClient->getBlob($containerName, $fileToUpload);
-	    fpassthru($blob->getContentStream());
-	    echo "<br />";
+	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		// echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		// echo $urlfile ;
+	} else {
+		$uploadOk = 0;
+		echo "Sorry, there was an error uploading your file.";
 	}
-	catch(ServiceException $e){
-	    $code = $e->getCode();
-	    $error_message = $e->getMessage();
-	    echo $code.": ".$error_message."<br />";
+
+	// $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+	// echo fread($myfile,filesize($fileToUpload));
+	// echo $myfile;
+	// fclose($myfile);
+	if ($uploadOk==1) {
+		$createContainerOptions = new CreateContainerOptions();
+		$createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+		$createContainerOptions->addMetaData("key1", "value1");
+		$createContainerOptions->addMetaData("key2", "value2");
+
+		$containerName = "blockblobs".generateRandomString();
+
+		try {
+			// Create container.
+			$blobClient->createContainer($containerName, $createContainerOptions);
+			// $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
+			// fclose($myfile);
+			
+			# Upload file as a block blob
+			// echo "Uploading BlockBlob: ".PHP_EOL;
+			// echo $fileToUpload;
+			// echo "<br />";
+			
+			$content = fopen($urlfile, "r");
+
+			//Upload blob
+			$blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+
+			// List blobs.
+			$listBlobsOptions = new ListBlobsOptions();
+			$listBlobsOptions->setPrefix($fileToUplloadWithoutExtension);
+			echo "<br/>";
+			
+			echo "These are the blobs present in the container : ";
+
+			do{
+				$result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+				foreach ($result->getBlobs() as $blob)
+				{
+					echo "<b>".$blob->getName()."</b> <br/>";
+					echo "Url name : <b>".$blob->getUrl()."</b> <br/>";
+				}			
+				$listBlobsOptions->setContinuationToken($result->getContinuationToken());
+			} while($result->getContinuationToken());
+			
+			echo "Name file : <b>".$fileToUplloadWithoutExtension."</b><br/>";
+			echo "<br />";
+
+			// Get blob.
+			// echo "This is the content of the blob uploaded: ";
+			// $blob = $blobClient->getBlob($containerName, $fileToUpload);
+			// fpassthru($blob->getContentStream());
+			// echo "<br />";
+		}
+		catch(ServiceException $e){
+			// Handle exception based on error codes and messages.
+			// Error codes and messages are here:
+			// http://msdn.microsoft.com/library/azure/dd179439.aspx
+			$code = $e->getCode();
+			$error_message = $e->getMessage();
+			echo $code.": ".$error_message."<br />";
+		}
+		catch(InvalidArgumentTypeException $e){
+			// Handle exception based on error codes and messages.
+			// Error codes and messages are here:
+			// http://msdn.microsoft.com/library/azure/dd179439.aspx
+			$code = $e->getCode();
+			$error_message = $e->getMessage();
+			echo $code.": ".$error_message."<br />";
+		}	
+
+	}else{
+		echo "Can't upload image";
 	}
-	catch(InvalidArgumentTypeException $e){
-	    $code = $e->getCode();
-	    $error_message = $e->getMessage();
-	    echo $code.": ".$error_message."<br />";
-	}
+}else{
+	try{
+        // Delete container.
+        echo "Deleting Container".PHP_EOL;
+        echo "<b>".$_GET["containerName"].PHP_EOL."</b>";
+        echo "<br />";
+        $blobClient->deleteContainer($_GET["containerName"]);
+    }
+    catch(ServiceException $e){
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
+}
 ?>
 
+<form method="post" action="uploadsystem.php?Cleanup&containerName=<?php echo $containerName; ?>">
+    <button type="submit">Press to clean up all resources created by this sample</button>
+</form>
+<a href="./"><button>Back</button></a>
